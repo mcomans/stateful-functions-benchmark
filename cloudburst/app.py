8f6b06fa51167d956f8125f7f367f01a59fc59b1
Martijn Comans <m.r.comans@student.tudelft.nl>
2021-10-12 11:45:17 +0200

    Add products functions

--- cloudburst/app.py
+++ cloudburst/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from benchmark import register
+from functions import register_functions
 from routes import orders, products, shopping_carts, users
 from cloud import cloud
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-register.register_functions(cloud)
+register_functions(cloud)
 
 app.register_blueprint(orders, url_prefix="/orders")
 app.register_blueprint(products, url_prefix="/products")
---
+++ cloudburst/functions/register.py
@@ -0,0 +1,20 @@
+from functions.products import register_products_functions
+
+def register_functions(cloud):
+  register_products_functions(cloud)
+
+  def add_to_cart(cb, cart_id, product_id, amount):
+    cart = cb.get(cart_id)
+
+    if cart is None:
+      cart = {}
+
+    if product_id in cart:
+      cart[product_id] += amount
+    else:
+      cart[product_id] = amount
+    
+    cb.put(cart_id, cart)
+    return str(cart)
+
+  cloud.register(add_to_cart, "add_to_cart")
