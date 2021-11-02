86af253ea42b23b07a3a88d0ccf8103d12713456
Martijn Comans <m.r.comans@student.tudelft.nl>
2021-11-02 16:17:58 +0100

    Cloudstate: Add product entity

--- cloudstate/stateful-functions/src/main/kotlin/Application.kt
+++ cloudstate/stateful-functions/src/main/kotlin/Application.kt
@@ -1,6 +1,24 @@
-class Application {
-}
+import io.cloudstate.kotlinsupport.cloudstate
+import product.Product
+import product.ProductEntity
+import product.persistence.Domain
 
 fun main() {
+    cloudstate {
+        config {
+            host = "0.0.0.0"
+            port = 8080
+            loglevel = "INFO"
+        }
 
+        eventsourced {
+            entityService = ProductEntity::class
+            descriptor = Product.getDescriptor().findServiceByName("ProductService")
+            additionalDescriptors = mutableListOf(Product.getDescriptor(), Domain.getDescriptor() )
+            snapshotEvery = 1
+            persistenceId = "product"
+        }
+    }.start()
+        .toCompletableFuture()
+        .get()
 }
---
+++ cloudstate/stateful-functions/src/main/kotlin/product/ProductEntity.kt
@@ -0,0 +1,41 @@
+package product
+
+import com.google.protobuf.Empty
+import io.cloudstate.javasupport.EntityId
+import io.cloudstate.javasupport.eventsourced.CommandContext
+import io.cloudstate.kotlinsupport.annotations.eventsourced.Snapshot
+import io.cloudstate.kotlinsupport.annotations.eventsourced.CommandHandler
+import io.cloudstate.kotlinsupport.annotations.eventsourced.EventHandler
+import io.cloudstate.kotlinsupport.annotations.eventsourced.EventSourcedEntity
+import io.cloudstate.kotlinsupport.annotations.eventsourced.SnapshotHandler
+import product.persistence.Domain
+
+
+@EventSourcedEntity
+class ProductEntity(@EntityId private val entityId: String) {
+    private var price: Int = 0
+    private var stock: Int = 0
+
+    @Snapshot
+    fun snapshot(): Domain.Product {
+        return Domain.Product.newBuilder().setPrice(price).setStock(stock).build()
+    }
+
+    @SnapshotHandler
+    fun snapshotHandler(product: Domain.Product) {
+        price = product.price
+        stock = product.stock
+    }
+
+
+    @EventHandler
+    fun priceChanged(priceChanged: Domain.PriceChanged) {
+        this.price = priceChanged.price
+    }
+
+    @CommandHandler
+    fun setPrice(price: Product.ProductPrice, ctx: CommandContext): Empty {
+        ctx.emit(Domain.PriceChanged.newBuilder().setPrice(price.price).build())
+        return Empty.getDefaultInstance()
+    }
+}
