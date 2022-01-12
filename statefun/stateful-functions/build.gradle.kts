6ff43f006a3a5c752b568d92e9d1a2dae7dd1b08
Martijn Comans <m.r.comans@student.tudelft.nl>
2022-01-12 09:54:12 +0100

    Statefun: Add deployment files

--- statefun/stateful-functions/build.gradle.kts
+++ statefun/stateful-functions/build.gradle.kts
@@ -30,6 +30,8 @@ tasks.withType<KotlinCompile>() {
 }
 
 tasks.withType<Jar>() {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
     manifest {
         attributes["Main-Class"] = "Expose"
     }
