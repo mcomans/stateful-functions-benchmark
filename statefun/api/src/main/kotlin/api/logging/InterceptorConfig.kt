cd3a310b7dc325500d0eaff6395a30dbd88115a0
Martijn Comans <m.r.comans@student.tudelft.nl>
2022-09-22 16:16:43 +0200

    Statefun: Add interceptor config bean

---
+++ statefun/api/src/main/kotlin/api/logging/InterceptorConfig.kt
@@ -0,0 +1,12 @@
+package api.logging
+
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Component
+class InterceptorConfig(private val loggingInterceptor: LoggingInterceptor) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(loggingInterceptor)
+    }
+}
--- statefun/api/src/main/kotlin/api/logging/LoggingInterceptor.kt
+++ statefun/api/src/main/kotlin/api/logging/LoggingInterceptor.kt
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Component
-class LoggingInterceptor() : HandlerInterceptor {
+class LoggingInterceptor : HandlerInterceptor {
     private val logger: Logger = LoggerFactory.getLogger(LoggingFilter::class.java)
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
